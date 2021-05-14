b62145aa7e97f91226ed362b9970a8c9fb28a1e3
Bijoy Sijo <bijoysijo21@gmail.com>
2021-05-14 18:07:26 +0530

    encouragements feature

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import discord
 from dotenv import load_dotenv
 import os
 import requests
+import json
+import random
 
 load_dotenv()
 
@@ -13,6 +15,9 @@ def get_quote():
   json_data = json.loads(response.text)
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
+starter_encouragements = ["Cheer up!", "Hang in there.", "You are a great person / bot!"]
 
 #intializer event
 @client.event
@@ -27,6 +32,9 @@ async def on_message(message):
   if message.content.startswith('!inspire'):
     quote = get_quote()
     await message.channel.send(quote)
+
+  if any(word in message.content for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
 
 #to establish connection with client using bot token
 client.run(os.getenv('TOKEN', 'Token not found'))
