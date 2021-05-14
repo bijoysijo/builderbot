dc9cd1d5d38ce8208e9ee551aac5dcd5b9f75bec
Bijoy Sijo <bijoysijo21@gmail.com>
2021-05-14 18:34:20 +0530

    add deta and insert user data function

--- bot.py
+++ bot.py
@@ -4,8 +4,16 @@ import os
 import requests
 import json
 import random
+from deta import Deta
 
 load_dotenv()
+
+#deta base
+deta = Deta(os.getenv('DETA_PROJECT_KEY'))
+
+#method to return db
+def getdb(name):
+  return deta.Base(name)
 
 #establish connection with discord client
 client = discord.Client()
@@ -15,6 +23,14 @@ def get_quote():
   json_data = json.loads(response.text)
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
+
+def update_encouragements_table(encouraging_message):
+  db = getdb("encouragements")
+  if db.get("encouragements"):
+    updates = {"cheers": db.util.append(encouraging_message)}
+    db.update(updates, "encouragements")
+  else:
+    db.put({"cheers": [encouraging_message]}, "encouragements")
 
 sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
 starter_encouragements = ["Cheer up!", "Hang in there.", "You are a great person / bot!"]
@@ -35,6 +51,11 @@ async def on_message(message):
 
   if any(word in message.content for word in sad_words):
     await message.channel.send(random.choice(starter_encouragements))
+
+  if message.content.startswith('!new'):
+    encouraging_message = message.content.split('!new ', 1)[1]
+    update_encouragements_table(encouraging_message)
+    await message.channel.send("New encouraging message added")
 
 #to establish connection with client using bot token
 client.run(os.getenv('TOKEN', 'Token not found'))
